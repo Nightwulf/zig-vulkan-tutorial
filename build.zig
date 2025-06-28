ede7956e01767820cf4dc26f2e8396f86e436d6d
Torsten Evers <tevers@onlinehome.de>
2025-06-28 23:46:35 +0200

    added run step and basic ZigBrains support

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const vertex_shader_compiler = b.addSystemCommand(&.{ "glslc", "-o" });
-    const vert_shader_out = vertex_shader_compiler.addOutputFileArg("frag.spv");
+    const vert_shader_out = vertex_shader_compiler.addOutputFileArg("vert.spv");
     vertex_shader_compiler.addFileArg(b.path("src/shader.vert" ));
     b.getInstallStep().dependOn(&b.addInstallBinFile(vert_shader_out, "vert.spv").step);
 
@@ -25,4 +25,11 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("vulkan");
     exe.linkSystemLibrary("glm");
     b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    run_exe.cwd = b.path("zig-out/bin");
+    run_exe.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the application");
+
+    run_step.dependOn(&run_exe.step);
 }
